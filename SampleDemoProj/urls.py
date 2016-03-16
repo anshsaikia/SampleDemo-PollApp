c54decacea5259cc5fdd268734f1734ef5b70932
anshsaikia <anshsaikia@example.com>
2016-03-16 17:15:08 +0530

    Modifued the index.html file

--- SampleDemoProj/urls.py
+++ SampleDemoProj/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from pollsApp.views import index
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'index', name='home'),
+    #url(r'^$', include('pollsApp.urls', namespace="polls")),
     # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('pollsApp.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
